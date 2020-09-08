654a959fbcc068b9a14d3a3020077acac0b8a576
Matheus <you@example.com>
2020-09-08 20:47:22 -0300

    primeiro commit

---
+++ calculos.js
@@ -0,0 +1,24 @@
+const LTDA = () => {
+    const readline = require('readline')
+    const fs = require('fs')
+    const readble = fs.createReadStream()
+    readble.line[0]
+
+  
+}
+
+let QualiNo = getElementById(QlN)
+let QualiOr = getElementById(QlO)
+let QuantDis = getElementById(QtD)
+let QuantCon = getElementById(QtC)
+
+if(QuliNo){
+
+}else if(QualiOr){
+
+}else if(QuantDis){
+
+}else if(QuantCon){
+
+}
+
